4fe05e8426c724f9bbc1af637c2a57cdfa6ad134
brody.sanford@gmail.com <brodysmac@Brodens-MBP.fios-router.home>
2022-05-19 08:17:58 -0400

    commit tree 1aaef1c530e74c1fe64c0b467ae97c3e7015f81e

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function practice(tutorials) {
+  let arrayOfLowerCaseTitles = tutorials.map(mixedCaseTitles => mixedCaseTitles.toLowerCase());
+//This part works to fix one title:
+/*
+  let title1 = arrayOfLowerCaseTitles[0].split(' ');
+  let fixedTitle1 = title1.map(function(word) {
+    return (word[0].toUpperCase() + word.slice(1))
+  });
+  return fixedTitle1.join(' ')
+*/
+//Now let's see if I can iterate it: 
+let splitLowercaseArray = arrayOfLowerCaseTitles.map(wholeTitle => wholeTitle.split(' '))
+let fixedArray = splitLowercaseArray.map(function(splitTitleArray){
+  return splitTitleArray.map(function(word) {
+    return (word[0].toUpperCase() + word.slice(1));
+  })
+})
+return fixedArray.map(fixedTitle => fixedTitle.join(' '))
+}
+
+
+function titleCased() {
+  let arraySplitTitles = tutorials.map(unfixedTitles => unfixedTitles.split(' '));
+  let fixedArray = arraySplitTitles.map(function(splitTitleArray){
+    return splitTitleArray.map(function(word) {
+      return (word.slice(0,1).toUpperCase() + word.slice(1));
+    })
+  })
+  return fixedArray.map(fixedTitle => fixedTitle.join(' '))
 }
